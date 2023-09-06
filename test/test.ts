556ad45e89cc42be0fb813590fe36e98f229ce43
adachi-codelic <adachi-t@codelic.co>
2023-09-06 17:37:12 +0900

    完成

--- test/test.ts
+++ test/test.ts
@@ -2,8 +2,8 @@ import * as prettier from 'prettier'
 const format = (code: any) => {
   const res = prettier.format(code, {
     parser: 'ts-parser',
-    plugins: ['./src/index.js']
+    plugins: ['./dist/index.js']
   })
   return res
 }
-console.log(await format(`(x) => parseInt(x);`))
+console.log(await format(`['1'].map((x) => parseInt(x))`))
